aea92636dfbd9e780716d53b853c3d82f5732a4e
braunyman <braun@mymail.mines.edu>
2020-02-14 21:21:14 -0700

    second part

--- src/com/ScoreTrakker.java
+++ src/com/ScoreTrakker.java
@@ -6,12 +6,12 @@ import java.util.*;
 
 public class ScoreTrakker {
 	private ArrayList<Student> studentList;
-	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt"};
-	
+	private String[] files = { "scores.txt", "badscore.txt", "nofile.txt" };
+
 	public ScoreTrakker() {
 		studentList = new ArrayList<Student>();
 	}
-	
+
 	private void loadDataFromFile(String fileName) {
 		FileReader in = null;
 		try {
@@ -25,36 +25,36 @@ public class ScoreTrakker {
 			int tempScore = 0;
 			String tempName = "";
 			String tempScoreString = "";
-			
+
 			tempName = scanner.nextLine();
 			tempScoreString = scanner.nextLine();
 			try {
 				tempScore = Integer.parseInt(tempScoreString);
-				studentList.add(new Student(tempName,tempScore));
+				studentList.add(new Student(tempName, tempScore));
 			} catch (NumberFormatException e) {
 				System.out.println("Incorrect format for " + tempName + ": not a valid score: " + tempScoreString);
 			}
-			
+
 		}
 		scanner.close();
-		
+
 	}
-	
+
 	private void printInOrder() {
 		Collections.sort(studentList);
 		System.out.println("Student Score List:");
-		studentList.forEach((k)-> System.out.println(k.toString()) );
+		studentList.forEach((k) -> System.out.println(k.toString()));
 	}
 
 	private void processFiles() {
-		for (String s: files) {
+		for (String s : files) {
 			loadDataFromFile(s);
 			printInOrder();
 			System.out.println();
 			studentList.clear();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		ScoreTrakker trakker = new ScoreTrakker();
 		trakker.processFiles();
